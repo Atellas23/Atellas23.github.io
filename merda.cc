6a308fc63883a9f0dcecd124fb0e7cf0a4e4d94c
Atellas23 <alex.batlle01@gmail.com>
2020-03-08 08:51:37 +0100

    reorganisation of files

--- merda.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-	for (int i = 0; i < 1000000; ++i) system("touch "+to_string(i).c_str()+".xd");
-}
